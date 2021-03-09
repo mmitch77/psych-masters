d850b1c78de4e33b0ef403eaa862a8076562aefe
mmitch77 <80299875+mmitch77@users.noreply.github.com>
2021-03-09 13:11:29 +1030

    Update weight-bias-iat.js
    Change to thin people

--- weight-bias-iat.js
+++ weight-bias-iat.js
@@ -5,7 +5,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 		category1 : {
 			name : 'Thin people', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Thin people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
